a762ffd36bfc1f39a556214c614cfb2ae2d8807c
Sepand Parhami <sparhami@google.com>
2019-03-11 12:02:01 -0700

    Pagination dots.

--- extensions/amp-inline-gallery/0.1/amp-inline-gallery-pagination.js
+++ extensions/amp-inline-gallery/0.1/amp-inline-gallery-pagination.js
@@ -19,6 +19,16 @@ import {setImportantStyles, setStyle} from '../../../src/style.js';
 import { getDetail } from '../../../src/event-helper';
 import { htmlFor } from '../../../src/static-template';
 
+/**
+ * Returns a number falling off from one to zero, based on a distance
+ * progress percentage and a power to decay at.
+ * @param {number} percentage
+ * @param {number} power
+ */
+function exponentialFalloff(percentage, power) {
+  return Math.max(0, 1 - (1 / Math.pow(percentage, power)));
+}
+
 export class AmpInlineGalleryPagination extends AMP.BaseElement {
   /** @param {!AmpElement} element */
   constructor(element) {
@@ -38,7 +48,7 @@ export class AmpInlineGalleryPagination extends AMP.BaseElement {
 
   /** @override */
   isLayoutSupported(layout) {
-    return true;
+    return layout == Layout.FIXED_HEIGHT;
   }
 
   /** @override */
@@ -56,7 +66,7 @@ export class AmpInlineGalleryPagination extends AMP.BaseElement {
         .pagination-dot {
           position: relative;
           overflow: hidden;
-          margin: 0 4px;
+          margin: 4px;
           background-color: #aaa;
         }
 
@@ -64,6 +74,8 @@ export class AmpInlineGalleryPagination extends AMP.BaseElement {
           position: absolute;
           top: 0;
           background-color: #333;
+          transform: scale(calc(1 - var(--percentage-falloff)));
+          opacity: calc(1 - var(--percentage-falloff));
         }
 
         .pagination-dot,
@@ -71,6 +83,27 @@ export class AmpInlineGalleryPagination extends AMP.BaseElement {
           width: 8px;
           height: 8px;
           border-radius: 50%;
+        }
+
+        :host([inset]) {
+          position: absolute !important;
+          left: 0;
+          right: 0;
+          bottom: calc(var(--amp-caption-margin-top) + var(--amp-caption-height, 0));
+          display: flex !important;
+          justify-content: center;
+          margin: 4px;
+        }
+
+        :host([inset]) .pagination-dot {
+          background-color: #bbb;
+          box-shadow: 0 0 3px rgba(0, 0, 0, 0.25),
+                      0 1px 1px rgba(0, 0, 0, 0.4);
+          mix-blend-mode: hard-light;
+        }
+
+        :host([inset]) .pagination-dot-progress {
+          background-color: #fff;
         }
       </style>
       <div class="pagination-dots" aria-hidden="true"></div>
@@ -108,19 +141,27 @@ export class AmpInlineGalleryPagination extends AMP.BaseElement {
     }
   }
 
+  updateDots_(index, offset) {
+    const position = index - offset;
+
+    Array.from(this.paginationDots_.children).forEach((dot, i) => {
+      const distance = i - position;
+      const percentage = Math.max(1 - Math.abs(distance), 0);
+      const percentageFalloff = exponentialFalloff(percentage, -0.5);
+
+      setImportantStyles(dot, {
+        '--percentage-falloff': percentageFalloff,
+      });
+    });
+  }
+
   handleIndexChangeUpdate_(event) {
     const detail = getDetail(event);
     const total = detail['total'];
     const index = detail['index'];
     const offset = detail['offset'];
-    const position = index - offset;
 
     this.updateTotal_(total);
-    Array.from(this.paginationDots_.children).forEach((dot, i) => {
-      const movement = (i - position) * 12;
-      const offset = `${movement}px`;
-      const progressEl = dot.firstElementChild;
-      setStyle(progressEl, 'right', offset);
-    });
+    this.updateDots_(index, offset);
   }
 }
